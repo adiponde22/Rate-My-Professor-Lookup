d52d9c63078398802ffdb5b4e0acdbe3d3d7c508
Aditya Ponde <94423956+adiponde22@users.noreply.github.com>
2023-01-31 14:31:43 -0500

    Add files via upload

--- lookup.js
+++ lookup.js
@@ -1,6 +1,8 @@
 // All rights reserved to Isabella Rasku and Aditya Ponde 2023.
 
 // making the context menu
+
+
 chrome.runtime.onInstalled.addListener(async () => {
     chrome.contextMenus.create({
       id: "Lookup",
@@ -11,13 +13,58 @@ chrome.runtime.onInstalled.addListener(async () => {
 
 });
 
+
+
+
 // opens RMP when context menu is clicked with the search perms using selected text
 chrome.contextMenus.onClicked.addListener((item, tab) => {
+  
   var name = item.selectionText;
   var uname = name.replace(/ /g, "+");
   let url = new URL("https://www.ratemyprofessors.com/search.jsp?queryBy=teacherName&schoolName=&queryoption=HEADER&")
   url.searchParams.set('query', uname)
-  chrome.tabs.create({ url: url.href, index: tab.index + 1 });
+
+  // gets resolution of the main window to open the popup in the right corner
+  chrome.windows.getCurrent((cw) => {
+    let w, h;
+    
+      w = cw.width - 500;
+    
+    // creates new popup window
+    chrome.windows.create(
+      
+    {
+
+      url: url.href,
+      type: "popup",
+      left: w,
+      width: 500,
+      height: 900,
+      
+
+    });
+
+    // gets ID of the popup window
+    chrome.windows.getCurrent((pop) => 
+    {
+
+      let popid = pop.id;
+
+      // when focus changes the window closes
+      chrome.windows.onFocusChanged.addListener( (stat) =>
+        {
+          chrome.windows.onFocusChanged.addListener( (stat) =>
+        {
+          chrome.windows.remove(popid);
+        });
+        });
+    });
+
+
+
+  });
+
+  
 
 });
 
